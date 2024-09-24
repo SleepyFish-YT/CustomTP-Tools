4d8b3d44a71a51bc395bf9d50331f9a3d25a476b
SleepyFish <151081838+SleepyFish-YT@users.noreply.github.com>
2024-09-24 22:19:18 +0200

    Delete src/main/java/me/sleepyfish/TPDupeChecker directory

--- src/main/java/me/sleepyfish/TPDupeChecker/libs/gson/annotations/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * This package provides annotations that can be used with {@link me.sleepyfish.TPNameEditor.libs.gson.Gson}.
- * 
- * @author Inderjeet Singh, Joel Leitch
- */
-package me.sleepyfish.TPDupeChecker.libs.gson.annotations;
